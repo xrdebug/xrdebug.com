806648eb4d9a0bcb92d3e86aceb42aff70578554
Rodolfo Berrios <20590102+rodber@users.noreply.github.com>
2024-02-22 08:00:24 -0300

    fix command display

--- godlike.js
+++ godlike.js
@@ -1,7 +1,7 @@
 document
     .querySelector("#command-install")
     .addEventListener("click", event => {
-        event.target.select();
-        // event.target.setSelectionRange(0, 99999);
-        // navigator.clipboard.writeText(event.target.value);
+        window.getSelection().selectAllChildren(
+            event.target
+        );
     });
